96c3d10fcf4cd0bf7f204ed194e9fee26697dbc4
YoonHyunWoo <yjs88zerg@gmail.com>
2025-07-26 00:25:29 +0900

    temp

--- internal/container/container.go
+++ internal/container/container.go
@@ -31,14 +31,12 @@ func Run() {
 }
 
 func Init() {
+
+	syscall.Kill(syscall.Getpid(), syscall.SIGSTOP)
+
 	fmt.Printf("Running: %v\n", os.Args[2:])
 
 	cgroup.SetupCgroups()
-
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
 
 	Must(syscall.Sethostname([]byte("container")))
 
@@ -51,8 +49,11 @@ func Init() {
 
 	defer Must(syscall.Unmount("proc", 0))
 	defer Must(syscall.Unmount("mytemp", 0))
-	// TODO : 멈춤 후 시그널 오면 시작
-	Must(cmd.Run())
+
+	if len(os.Args) < 3 {
+		log.Fatal("Usage: containeruntime")
+	}
+	syscall.Exec(os.Args[2], os.Args[3:], os.Environ())
 }
 
 func Must(err error) {
