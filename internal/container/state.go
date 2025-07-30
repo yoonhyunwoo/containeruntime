967fb36404be025f2feb0003b89ae6a317f424e8
YoonHyunWoo <yjs88zerg@gmail.com>
2025-07-31 00:25:52 +0900

    state 관리 함수 init

--- internal/container/state.go
+++ internal/container/state.go
@@ -1,9 +1,46 @@
 package container
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/opencontainers/runtime-spec/specs-go"
 )
 
-func Temp() {
-	_ = specs.State{}
+const contaeruntimeStateDir string = "/run/containeruntime"
+
+func initStateDir() error {
+	return os.MkdirAll(contaeruntimeStateDir, 0755)
+}
+
+func getStatePath(containerId string) string {
+	return filepath.Join(contaeruntimeStateDir, containerId+".json")
+}
+
+func saveState(state *specs.State) error {
+	return nil
+}
+
+func loadState(containerId string) (*specs.State, error) {
+	return nil, nil
+}
+
+func deleteState(containerId string) error {
+	return nil
+}
+
+func listState(containerId string) ([]*specs.State, error) {
+	return nil, nil
+}
+
+func newContainerState(id, bundlePath string) (*specs.State, error) {
+	return nil, nil
+}
+
+func updateContainerState(containerId string, state *specs.State) error {
+	return nil
+}
+
+func setContainerPID(containerId string, pid int) error {
+	return nil
 }
