2f48875dc165a72833b9101dd3ee992320b94ffb
yoonhyunwoo <yjs88zerg@gmail.com>
2025-07-31 14:26:44 +0900

    feat : implement state manage functions

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/yoonhyunwoo/containeruntime
 go 1.24.4
 
 require (
-	github.com/opencontainers/runtime-spec v1.2.1 // indirect
-	github.com/urfave/cli/v3 v3.3.8 // indirect
+	github.com/opencontainers/runtime-spec v1.2.1
+	github.com/urfave/cli/v3 v3.3.8
 )
--- internal/container/state.go
+++ internal/container/state.go
@@ -1,46 +1,113 @@
 package container
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/opencontainers/runtime-spec/specs-go"
 )
 
-const contaeruntimeStateDir string = "/run/containeruntime"
+const (
+	containeruntimeStateDir string = "/run/containeruntime"
+)
 
 func initStateDir() error {
-	return os.MkdirAll(contaeruntimeStateDir, 0755)
+	return os.MkdirAll(containeruntimeStateDir, 0755)
 }
 
 func getStatePath(containerId string) string {
-	return filepath.Join(contaeruntimeStateDir, containerId+".json")
+	return filepath.Join(containeruntimeStateDir, containerId+".json")
 }
 
 func saveState(state *specs.State) error {
-	return nil
+	statePath := getStatePath(state.ID)
+	tempPath := statePath + ".tmp"
+
+	f, err := os.Create(tempPath)
+	if err != nil {
+		return fmt.Errorf("Can not save state: %v", err)
+	}
+	defer f.Close()
+	defer os.Remove(tempPath)
+
+	if err := json.NewEncoder(f).Encode(state); err != nil {
+		return fmt.Errorf("Can not parsing state : %v", err)
+	}
+
+	f.Close()
+	return os.Rename(tempPath, statePath)
 }
 
 func loadState(containerId string) (*specs.State, error) {
-	return nil, nil
+	statePath := getStatePath(containerId)
+	state := &specs.State{}
+
+	f, err := os.Open(statePath)
+	if err != nil {
+		return nil, fmt.Errorf("Can not open state: %v", err)
+	}
+	defer f.Close()
+
+	if err = json.NewDecoder(f).Decode(state); err != nil {
+		return nil, fmt.Errorf("Can not parsing state : %v", err)
+	}
+
+	return state, nil
 }
 
 func deleteState(containerId string) error {
+	statePath := getStatePath(containerId)
+	if err := os.Remove(statePath); err != nil {
+		return fmt.Errorf("Can not delete state: %v", err)
+	}
 	return nil
 }
 
 func listState(containerId string) ([]*specs.State, error) {
-	return nil, nil
+	var states []*specs.State
+	files, err := os.ReadDir(containeruntimeStateDir)
+	if err != nil {
+		return nil, fmt.Errorf("Can not list state: %v", err)
+	}
+
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".json") {
+			state, err := loadState(file.Name())
+			if err != nil {
+				continue
+			}
+			states = append(states, state)
+		}
+	}
+
+	return states, nil
 }
 
 func newContainerState(id, bundlePath string) (*specs.State, error) {
-	return nil, nil
-}
-
-func updateContainerState(containerId string, state *specs.State) error {
-	return nil
+	state := &specs.State{
+		Version:     specs.Version,
+		ID:          id,
+		Status:      specs.StateCreating,
+		Pid:         0,
+		Bundle:      bundlePath,
+		Annotations: nil,
+	}
+	return state, nil
 }
 
 func setContainerPID(containerId string, pid int) error {
+	state, err := loadState(containerId)
+	if err != nil {
+		return fmt.Errorf("Can set container pid: %v", err)
+	}
+
+	state.Pid = pid
+	if err := saveState(state); err != nil {
+		return fmt.Errorf("Can set container pid: %v", err)
+	}
+
 	return nil
 }
