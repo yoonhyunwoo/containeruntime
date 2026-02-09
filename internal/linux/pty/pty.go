b0641e39cf173be34dd7a211d2ae60004ec1c6ea
YoonHyunWoo <yjs88zerg@gmail.com>
2026-02-09 17:34:17 +0900

    fix(pty): call ioctl via SYS_IOCTL and unlock ptmx

--- internal/linux/pty/pty.go
+++ internal/linux/pty/pty.go
@@ -7,18 +7,32 @@ import (
 	"unsafe"
 )
 
+func ioctl(fd uintptr, req, arg uintptr) error {
+	if _, _, errno := syscall.Syscall(syscall.SYS_IOCTL, fd, req, arg); errno != 0 {
+		return errno
+	}
+	return nil
+}
+
 func PtyPair() (masterPty, slavePty *os.File, err error) {
 	masterPty, err = os.Open("/dev/ptmx")
 	if err != nil {
 		return nil, nil, fmt.Errorf("pty: failed to open /dev/ptmx: %w", err)
 	}
 
-	var ptn uint32
-	// #nosec G103 -- ioctl TIOCGPTN requires pointer passing to kernel for PTY index retrieval.
-	if _, _, errno := syscall.Syscall(syscall.TIOCGPTN, masterPty.Fd(), uintptr(unsafe.Pointer(&ptn)), 0); errno != 0 {
+	var unlock int32
+	if err = ioctl(masterPty.Fd(), syscall.TIOCSPTLCK, uintptr(unsafe.Pointer(&unlock))); err != nil {
 		_ = masterPty.Close()
-		return nil, nil, fmt.Errorf("pty: failed to get slave pty number: %w", errno)
+		return nil, nil, fmt.Errorf("pty: failed to unlock ptmx: %w", err)
 	}
+
+	var ptn uint32
+	// #nosec G103 -- ioctl requires pointer passing to kernel for PTY index retrieval.
+	if err = ioctl(masterPty.Fd(), syscall.TIOCGPTN, uintptr(unsafe.Pointer(&ptn))); err != nil {
+		_ = masterPty.Close()
+		return nil, nil, fmt.Errorf("pty: failed to get slave pty number: %w", err)
+	}
+
 	slaveName := fmt.Sprintf("/dev/pts/%d", ptn)
 	slavePty, err = os.OpenFile(slaveName, os.O_RDWR, 0)
 	if err != nil {
